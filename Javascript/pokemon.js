5f04edd56b7c7faf03caa4c86a9a8cc0bd092e57
Ari-rizz <arian.rizzuti@gokstadakademiet.no>
2024-04-02 14:05:52 +0200

    Lagde funksjon som samler Navn bilde og type

--- Javascript/pokemon.js
+++ Javascript/pokemon.js
@@ -1,16 +1,33 @@
 // Fetch som henter pokemon
 async function fetchPokemon() {
-    try {
-        const response = await fetch(
-            "https://pokeapi.co/api/v2/pokemon/?limit=50"
-        );
-        const data = await response.json();
-        const pokemonList = data.results;
-        makePokemonArray(pokemonList);
-    } catch (error) {
-        console.log("Kunne ikke laste inn pokemon: " + error);
-    }
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=50");
+    const data = await response.json();
+    const pokemonList = data.results;
+    makePokemonArray(pokemonList);
+  } catch (error) {
+    console.log("Kunne ikke laste inn pokemon: " + error);
+  }
 }
-
-function showPokemon(pokemon){}
-fetchPokemon()
+// Funksjon som lager pokemon array med de forskjellige objektene vi vil ha med
+async function makePokemonArray(pokemonList) {
+  try {
+    for (let i = 0; i < pokemonList.length; i++) {
+      const response = await fetch(pokemonList[i].url);
+      const data = await response.json();
+      let pokemon = {
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`, // spirits = bilde til pokemonen. ${i + 1} brukes for å oppnå riktig bilde per pokemon
+        name: pokemonList[i].name,
+        type: data.types[0].type.name, // velger kun den første type-en til pokemonen
+      };
+      showPokemon(pokemon);
+    }
+    console.log(pokemonList);
+  } catch (error) {
+    console.log("Kunne ikke laste inn pokemon" + error);
+  }
+}
+function showPokemon(pokemon) {
+    console.log(pokemon)
+}
+fetchPokemon();
